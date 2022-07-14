bf4d3ac45c9833a1b2c7f136305e4575b87a14cd
“Fernando” <myasas2014@hotmail.com>
2022-07-14 17:43:52 -0500

    commit Nro 14 julio 2022

--- main.go
+++ main.go
@@ -1,10 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func find(item string, slice []string) bool {
+	for _, sliceItem := range slice {
+		if item == sliceItem {
+			return true
+		} // retorna verdadero si la cadena se encuentra en el slice
+	} // fin forange
+	return false
+}
+
+type Refrigerador []string
+
+func (r Refrigerador) Open() {
+	fmt.Println(" Opening  refrigerator")
+}
+
+func (r Refrigerador) Close() {
+	fmt.Println(" Closing refrigerator")
+}
+
+func (r Refrigerador) FindFood(food string) error {
+	r.Open()
+	defer r.Close()
+	if find(food, r) {
+		fmt.Println("Found ", food)
+	} else {
+		return fmt.Errorf(" %s not found ", food)
+	}
+	//defer r.Close()
+	return nil
+}
 
 func main() {
-	name := "Go Developers"
-	salida := "resultado ejemplo"
-	fmt.Println("Azure for", name, salida)
+	refrigerador := Refrigerador{"leche", "pizza", "salsa"}
+
+	for _, food := range []string{"leche", "Bananas"} {
+		error := refrigerador.FindFood(food)
+		if error != nil {
+			log.Fatal(error)
+		}
+	}
 
 }
